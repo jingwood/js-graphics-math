adfea0c4c05b8b91749b2678f766297dbbc8bf00
Jingwood <dujid0@gmail.com>
2019-12-09 22:52:52 +0900

    add simple test framework

---
+++ test/framework.js
@@ -0,0 +1,66 @@
+
+function compare(a, b) {
+  if (typeof a.equals === "function") {
+    return a.equals(b);
+  } else {
+    return a == b;
+  }
+}
+
+class _TestSuite {
+  constructor(name) {
+    this.name = name;
+    this.cases = [];
+
+    this._currentTestCase = null;
+
+    this.tested = 0;
+    this.successed = 0;
+    this.failed = 0;
+  }
+
+  test(name, callback) {
+    if (this.tested === 0) {
+      console.log(`test suite ${this.name}:`);
+    }
+
+    this._currentTestName = name;
+    this._currentTestSuccess = true;
+
+    try {
+      callback(this);
+    } catch (ex) {
+      console.log(` - '${this._currentTestName}' failed: ${ex}`);
+      this._currentTestSuccess = false;
+    }
+     
+    this.tested++;
+   
+    if (this._currentTestSuccess) {
+      this.successed++;
+    } else {
+      this.failed++;
+    }
+    
+    return this;
+  }
+  
+  stats() {
+    console.log(` done ${this.tested} tested, ${this.successed} successed, ${this.failed} failed.\n`);
+  }
+
+  assert(ret, expect = true) {
+    const res = compare(ret, expect);
+  
+    if (!res) {
+      this._currentTestSuccess = false;
+      console.log(` - '${this._currentTestName}' failed, expect ${expect} but ${ret}`);
+    }
+  }
+}
+
+function testSuite(name, cases) {
+  return new _TestSuite(name, cases);
+}
+
+export { testSuite };
