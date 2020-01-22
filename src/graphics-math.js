ee57644199d466e2ab3a914eb4a1b9d52dc169b6
Jingwood <dujid0@gmail.com>
2020-01-22 11:59:34 +0900

    add entry main js

---
+++ src/graphics-math.js
@@ -0,0 +1,13 @@
+
+export { BBox2D } from "./bbox2.js";
+export { BBox3D } from "./bbox3.js";
+export { Color3 } from "./color3.js";
+export { Color4 } from "./color4.js";
+export { MathFunctions } from "./functions.js";
+export { Matrix3 } from "./matrix3.js";
+export { Matrix4 } from "./matrix4.js";
+export { Ray } from "./ray.js";
+export { } from "./utility.js";
+export { Vec2 } from "./vec2.js";
+export { Vec3 } from "./vec3.js";
+export { Vec4 } from "./vec4.js";
